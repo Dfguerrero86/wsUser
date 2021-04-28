e52584ec6025822e024ba5cc62bf9d358c9861b8
Danny Guerrero <dguerrero@redesis.com>
2021-04-27 21:25:51 -0500

    Versión inicial

---
+++ IService1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace wsUser
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        Int64 getCountPersonas();
+
+        [OperationContract]
+        String getApellidoPersona(String documento);
+    }
+
+    [DataContract]
+    public class Persona
+    {
+        private string tipoDocumento;
+        private string numeroDocumento;
+        private string grupoSanguineo;
+        private string sexo;
+        private string primerApellido;
+        private string segundoApellido;
+        private string primerNombre;
+        private string segundoNombre;
+        private string domicilio;
+        private string telefono;
+        private string fechaNacimiento;
+        private string correoElectronico;
+        private string correoElectronicoAlterno;
+        private string celular;
+        private string estadoCivil;
+
+        [DataMember]
+        public string TipoDocumento
+        {
+            get { return tipoDocumento; }
+            set { tipoDocumento = value; }
+        }
+        [DataMember]
+        public string NumeroDocumento
+        {
+            get { return numeroDocumento; }
+            set { numeroDocumento = value; }
+        }
+        [DataMember]
+        public string GrupoSanguineo
+        {
+            get { return grupoSanguineo; }
+            set { grupoSanguineo = value; }
+        }
+        [DataMember]
+        public string Sexo
+        {
+            get { return sexo; }
+            set { sexo = value; }
+        }
+        [DataMember]
+        public string PrimerApellido
+        {
+            get { return primerApellido; }
+            set { primerApellido = value; }
+        }
+        [DataMember]
+        public string SegundoApellido
+        {
+            get { return segundoApellido; }
+            set { segundoApellido = value; }
+        }
+        [DataMember]
+        public string PrimerNombre
+        {
+            get { return primerNombre; }
+            set { primerNombre = value; }
+        }
+        [DataMember]
+        public string SegundoNombre
+        {
+            get { return segundoNombre; }
+            set { segundoNombre = value; }
+        }
+        [DataMember]
+        public string Domicilio
+        {
+            get { return domicilio; }
+            set { domicilio = value; }
+        }
+        [DataMember]
+        public string Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+        [DataMember]
+        public string FechaNacimiento
+        {
+            get { return fechaNacimiento; }
+            set { fechaNacimiento = value; }
+        }
+        [DataMember]
+        public string CorreoElectronico
+        {
+            get { return correoElectronico; }
+            set { correoElectronico = value; }
+        }
+        [DataMember]
+        public string CorreoElectronicoAlterno
+        {
+            get { return correoElectronicoAlterno; }
+            set { correoElectronicoAlterno = value; }
+        }
+        [DataMember]
+        public string Celular
+        {
+            get { return celular; }
+            set { celular = value; }
+        }
+        [DataMember]
+        public string EstadoCivil
+        {
+            get { return estadoCivil; }
+            set { estadoCivil = value; }
+        }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Npgsql;
+
+namespace wsUser
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public Int64 getCountPersonas()
+        {
+            string connectionString = "Server=127.0.0.1; Port=5432;Database=informatica;User Id=postgres;Password=admin;";
+            NpgsqlConnection conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            string sql = "SELECT count(*) FROM persona";
+            var cmd = new NpgsqlCommand(sql, conn);
+            Int64 countPersonas=(Int64)cmd.ExecuteScalar();
+            conn.Close();
+            return countPersonas;
+        }
+
+        public String getApellidoPersona(String documento)
+        {
+            string connectionString = "Server=127.0.0.1; Port=5432;Database=informatica;User Id=postgres;Password=admin;";
+            NpgsqlConnection conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            string sql = "SELECT primer_apellido FROM persona where numero_documento='"+documento+"'";
+            var cmd = new NpgsqlCommand(sql, conn);
+            String apellidoPersona = "";
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+                apellidoPersona=dr[0].ToString();
+            conn.Close();
+            return apellidoPersona;
+        }
+    }
+}
